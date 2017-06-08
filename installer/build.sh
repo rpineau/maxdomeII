937a44b4fbfee667a1e4a793f45dcaeab351a549
Rodolphe Pineau <pineau@rti-zone.org>
2017-06-07 18:56:19 -0700

    fix directory name

--- installer/build.sh
+++ installer/build.sh
@@ -7,10 +7,10 @@ cp "../build/Release/libmaxdomeII.dylib" ROOT/tmp/maxdomeII_X2/
 
 if [ ! -z "$installer_signature" ]; then
 # signed package using env variable installer_signature
-pkgbuild --root ROOT --identifier org.rti-zone.maxdomeII_X2 --sign "$installer_signature" --scripts Scritps --version 1.0 maxdomeII_X2.pkg
+pkgbuild --root ROOT --identifier org.rti-zone.maxdomeII_X2 --sign "$installer_signature" --scripts Scripts --version 1.0 maxdomeII_X2.pkg
 pkgutil --check-signature ./maxdomeII_X2.pkg
 else
-pkgbuild --root ROOT --identifier org.rti-zone.maxdomeII_X2 --scripts Scritps --version 1.0 maxdomeII_X2.pkg
+pkgbuild --root ROOT --identifier org.rti-zone.maxdomeII_X2 --scripts Scripts --version 1.0 maxdomeII_X2.pkg
 fi
 
 rm -rf ROOT
