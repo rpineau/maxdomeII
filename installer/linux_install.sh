b7efbc2046f9f063e6ddf24bab5bac8e5499c541
Rodolphe Pineau <pineau@rti-zone.org>
2018-11-23 18:31:27 -0800

    fix linux install script as apparently the new plugin directory will be PlugIns64

--- installer/linux_install.sh
+++ installer/linux_install.sh
@@ -6,9 +6,7 @@ echo "TheSkyX_Install = $TheSkyX_Install"
 if [ ! -f "$TheSkyX_Install" ]; then
     echo TheSkyXInstallPath.txt not found
     TheSkyX_Path=`/usr/bin/find ~/ -maxdepth 3 -name TheSkyX`
-    if [ -d "$TheSkyX_Path" ]; then
-		TheSkyX_Path="${TheSkyX_Path}/Contents"
-    else
+    if [ ! -d "$TheSkyX_Path" ]; then
 	   echo TheSkyX application was not found.
     	exit 1
 	 fi
@@ -24,15 +22,21 @@ if [ ! -d "$TheSkyX_Path" ]; then
     exit 1
 fi
 
+if [ -d "$TheSkyX_Path/Resources/Common/PlugIns64" ]; then
+	PLUGINS_DIR="PlugIns64"
+else
+	PLUGINS_DIR="PlugIns"
+fi
+
 cp "./domelist MaxDomeII.txt" "$TheSkyX_Path/Resources/Common/Miscellaneous Files/"
-cp "./maxdomeII.ui" "$TheSkyX_Path/Resources/Common/PlugIns/DomePlugIns/"
-cp "./libmaxdomeII.so" "$TheSkyX_Path/Resources/Common/PlugIns/DomePlugIns/"
+cp "./maxdomeII.ui" "$TheSkyX_Path/Resources/Common/$PLUGINS_DIR/DomePlugIns/"
+cp "./libmaxdomeII.so" "$TheSkyX_Path/Resources/Common/$PLUGINS_DIR/DomePlugIns/"
 
 app_owner=`/usr/bin/stat -n -f "%u" "$TheSkyX_Path" | xargs id -n -u`
 if [ ! -z "$app_owner" ]; then
 	chown $app_owner "$TheSkyX_Path/Resources/Common/Miscellaneous Files/domelist MaxDomeII.txt"
-	chown $app_owner "$TheSkyX_Path/Resources/Common/PlugIns/DomePlugIns/maxdomeII.ui"
-	chown $app_owner "$TheSkyX_Path/Resources/Common/PlugIns/DomePlugIns/libmaxdomeII.so"
+	chown $app_owner "$TheSkyX_Path/Resources/Common/$PLUGINS_DIR/DomePlugIns/maxdomeII.ui"
+	chown $app_owner "$TheSkyX_Path/Resources/Common/$PLUGINS_DIR/DomePlugIns/libmaxdomeII.so"
 fi
-chmod  755 "$TheSkyX_Path/Resources/Common/PlugIns/DomePlugIns/libmaxdomeII.so"
+chmod  755 "$TheSkyX_Path/Resources/Common/$PLUGINS_DIR/DomePlugIns/libmaxdomeII.so"
 
