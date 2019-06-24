ec3ee6f8506914fad2a972d4756475e0a64500c0
Rodolphe Pineau <pineau@rti-zone.org>
2019-06-23 17:51:07 -0700

    collapse install script for linux and arm

--- installer/install.sh
+++ installer/install.sh
@@ -6,9 +6,7 @@ echo "TheSkyX_Install = $TheSkyX_Install"
 if [ ! -f "$TheSkyX_Install" ]; then
     echo TheSkyXInstallPath.txt not found
     TheSkyX_Path=`/usr/bin/find ~/ -maxdepth 3 -name TheSkyX`
-    if [ -d "$TheSkyX_Path" ]; then
-		TheSkyX_Path="${TheSkyX_Path}/Contents"
-    else
+    if [ ! -d "$TheSkyX_Path" ]; then
 	   echo TheSkyX application was not found.
     	exit 1
 	 fi
@@ -24,15 +22,25 @@ if [ ! -d "$TheSkyX_Path" ]; then
     exit 1
 fi
 
+if [ -d "$TheSkyX_Path/Resources/Common/PlugIns64" ]; then
+	PLUGINS_DIR="PlugIns64"
+else [ -d "$TheSkyX_Path/Resources/Common/PlugInsARM32" ]; then
+	PLUGINS_DIR="PlugInsARM32"
+else [ -d "$TheSkyX_Path/Resources/Common/PlugInsARM64" ]; then
+	PLUGINS_DIR="PlugInsARM32"
+else
+	PLUGINS_DIR="PlugIns"
+fi
+
 cp "./domelist MaxDomeII.txt" "$TheSkyX_Path/Resources/Common/Miscellaneous Files/"
-cp "./maxdomeII.ui" "$TheSkyX_Path/Resources/Common/PlugInsARM32/DomePlugIns/"
-cp "./libmaxdomeII.so" "$TheSkyX_Path/Resources/Common/PlugInsARM32/DomePlugIns/"
+cp "./maxdomeII.ui" "$TheSkyX_Path/Resources/Common/$PLUGINS_DIR/DomePlugIns/"
+cp "./libmaxdomeII.so" "$TheSkyX_Path/Resources/Common/$PLUGINS_DIR/DomePlugIns/"
 
 app_owner=`/usr/bin/stat -c "%u" "$TheSkyX_Path" | xargs id -n -u`
 if [ ! -z "$app_owner" ]; then
 	chown $app_owner "$TheSkyX_Path/Resources/Common/Miscellaneous Files/domelist MaxDomeII.txt"
-	chown $app_owner "$TheSkyX_Path/Resources/Common/PlugInsARM32/DomePlugIns/maxdomeII.ui"
-	chown $app_owner "$TheSkyX_Path/Resources/Common/PlugInsARM32/DomePlugIns/libmaxdomeII.so"
+	chown $app_owner "$TheSkyX_Path/Resources/Common/$PLUGINS_DIR/DomePlugIns/maxdomeII.ui"
+	chown $app_owner "$TheSkyX_Path/Resources/Common/$PLUGINS_DIR/DomePlugIns/libmaxdomeII.so"
 fi
-chmod  755 "$TheSkyX_Path/Resources/Common/PlugInsARM32/DomePlugIns/libmaxdomeII.so"
+chmod  755 "$TheSkyX_Path/Resources/Common/$PLUGINS_DIR/DomePlugIns/libmaxdomeII.so"
 
