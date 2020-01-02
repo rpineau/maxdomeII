a023b4dc304f418fa8f7deaeba193a14e30cfcea
Rodolphe Pineau <pineau@rti-zone.org>
2020-01-02 13:27:31 -0800

    Fix macOS 64 bit code signing

--- installer/build.sh
+++ installer/build.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
+PACKAGE_NAME="maxdomeII_X2.pkg"
+BUNDLE_NAME="org.rti-zone.maxdomeIIX2"
+
+if [ ! -z "$app_id_signature" ]; then
+    codesign -f -s "$app_id_signature" --verbose ../build/Release/libmaxdomeII.dylib
+fi
+
 mkdir -p ROOT/tmp/maxdomeII_X2/
 cp "../maxdomeII.ui" ROOT/tmp/maxdomeII_X2/
 cp "../domelist MaxDomeII.txt" ROOT/tmp/maxdomeII_X2/
 cp "../build/Release/libmaxdomeII.dylib" ROOT/tmp/maxdomeII_X2/
 
+
 if [ ! -z "$installer_signature" ]; then
-# signed package using env variable installer_signature
-pkgbuild --root ROOT --identifier org.rti-zone.maxdomeII_X2 --sign "$installer_signature" --scripts Scripts --version 1.0 maxdomeII_X2.pkg
-pkgutil --check-signature ./maxdomeII_X2.pkg
+	# signed package using env variable installer_signature
+	pkgbuild --root ROOT --identifier $BUNDLE_NAME --sign "$installer_signature" --scripts Scripts --version 1.0 $PACKAGE_NAME
+	pkgutil --check-signature ./${PACKAGE_NAME}
 else
-pkgbuild --root ROOT --identifier org.rti-zone.maxdomeII_X2 --scripts Scripts --version 1.0 maxdomeII_X2.pkg
+	pkgbuild --root ROOT --identifier $BUNDLE_NAME --scripts Scripts --version 1.0 $PACKAGE_NAME
 fi
 
 rm -rf ROOT
